afca748755429164f9552d81e4dc2d793e214500
Tiago Lopes <tflprofissional@gmail.com>
2022-11-23 16:01:14 +0000

    modified:   frontend/package.json 	modified:   frontend/pnpm-lock.yaml 	deleted:    frontend/src/App.jsx 	deleted:    frontend/src/components/Header.css 	deleted:    frontend/src/components/Header.jsx 	deleted:    frontend/src/components/Navbar.css 	deleted:    frontend/src/components/Navbar.jsx 	modified:   frontend/src/index.css 	modified:   frontend/src/main.jsx 	renamed:    frontend/src/App.css -> frontend/src/pages/App.css 	new file:   frontend/src/pages/App.jsx 	new file:   frontend/src/pages/Error.css 	new file:   frontend/src/pages/Error.jsx 	new file:   frontend/src/pages/Signup.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./pages/App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Error from "./pages/Error";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
---
+++ frontend/src/pages/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="App flex justify-center items-center">TESTE</div>;
+}
+
+export default App;
